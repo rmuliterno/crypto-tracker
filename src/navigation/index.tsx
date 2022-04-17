7e7e3df678439dc806c1706ee3269950896476a3
rmuliterno <renan-mf1@hotmail.com>
2022-04-17 17:41:18 -0300

    feat: adding icons

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,21 +1,46 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Detail from '../screens/Detail';
 import Home from '../screens/Home';
 import List from '../screens/List';
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="List" component={List} />
-        <Stack.Screen name="Detail" component={Detail} />
-      </Stack.Navigator>
+      <Tab.Navigator 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+                iconName = focused? 'ios-home-sharp': 'ios-home-outline';
+            } else if (route.name === 'Favourites') {
+                iconName = focused ? 'ios-heart-sharp' : 'ios-heart-outline';
+            }
+
+            console.log(iconName)
+           
+            // You can return any component that you like here!
+            return <Icon name='users' size={60} color='#900' />
+        },
+          tabBarActiveTintColor: '#58ceb2',
+          tabBarInactiveTintColor: 'gray',
+          //Tab bar styles can be added here
+          tabBarStyle:{paddingVertical: 5,borderTopLeftRadius:15,borderTopRightRadius:15,backgroundColor:'white',position:'absolute',height:200},
+          tabBarLabelStyle:{paddingBottom:3},
+      })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="List" component={List} />
+        <Tab.Screen name="Detail" component={Detail} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import {TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import StateContext from '../../contexts/state';
 import {Container, Text} from './styles';
@@ -19,6 +20,7 @@ const Home = props => {
 
   return (
     <Container>
+      <Icon name='users' size={60} color='#900' />
       <Text>Home2</Text>
       <TouchableOpacity onPress={handlePress}>
         <Text>Go to List</Text>
